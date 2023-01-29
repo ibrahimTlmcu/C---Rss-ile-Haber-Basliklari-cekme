5f18a048c4c6b78fb612c6758e25c39bf9f4e4ce
İbrahim TULUMCU <94108626+ibrahimTlmcu@users.noreply.github.com>
2023-01-29 19:00:01 +0300

    Add files via upload

---
+++ RSSileVeriCekme/RSSileVeriCekme/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace RSSileVeriCekme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Getir = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Temizle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Getir
+            // 
+            this.Getir.Location = new System.Drawing.Point(12, 254);
+            this.Getir.Name = "Getir";
+            this.Getir.Size = new System.Drawing.Size(120, 44);
+            this.Getir.TabIndex = 0;
+            this.Getir.Text = "Getir";
+            this.Getir.UseVisualStyleBackColor = true;
+            this.Getir.Click += new System.EventHandler(this.Getir_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(474, 212);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Temizle
+            // 
+            this.Temizle.Location = new System.Drawing.Point(366, 254);
+            this.Temizle.Name = "Temizle";
+            this.Temizle.Size = new System.Drawing.Size(120, 44);
+            this.Temizle.TabIndex = 2;
+            this.Temizle.Text = "Temizle";
+            this.Temizle.UseVisualStyleBackColor = true;
+            this.Temizle.Click += new System.EventHandler(this.Temizle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(498, 319);
+            this.Controls.Add(this.Temizle);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Getir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Getir;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Temizle;
+    }
+}
+
---
+++ RSSileVeriCekme/RSSileVeriCekme/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace RSSileVeriCekme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Getir_Click(object sender, EventArgs e)
+        {
+            XmlTextReader xmloklu = new XmlTextReader("https://www.cumhuriyet.com.tr/rss/son_dakika.xml");
+            while (xmloklu.Read())
+            {
+                if(xmloklu.Name == "title")
+                {
+                    listBox1.Items.Add(xmloklu.ReadString());   
+ 
+                }
+                
+            }
+        }
+
+        private void Temizle_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            
+        }
+    }
+}
